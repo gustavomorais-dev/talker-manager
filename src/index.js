f1922d801fcd259f96dc51ca310592499da0c273
gustavomorais <gustavomorais.dev@gmail.com>
2023-07-05 16:53:57 -0300

    completa requisito 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -11,11 +12,26 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-// Endpoint para obter todas as pessoas palestrantes
+// 1 - Endpoint para obter todas as pessoas palestrantes
 app.get('/talker', async (_request, response) => {
   const data = await readTalkersFile();
   response.status(HTTP_OK_STATUS).json(data);
 });
+
+// 2 - Endpoint para obter uma pessoa palestrante com base no ID
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await readTalkersFile();
+
+  const talker = talkers.find((talker) => talker.id === Number(id));
+
+  if (talker) {
+    response.status(HTTP_OK_STATUS).json(talker);
+  } else {
+    response.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log('Online');
